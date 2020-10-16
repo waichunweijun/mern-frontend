07fa7e3c03fa9e297946d803a6c4654dede75677
waichun <waichun@pixiumdigital.com>
2020-10-16 17:52:27 +0800

    added authentication page

--- src/user/pages/Authenticate.js
+++ src/user/pages/Authenticate.js
@@ -1,34 +1,64 @@
 import React from 'react';
+
+import Card from '../../shared/components/UIElement/Card';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import {
-    VALIDATOR_REQUIRE,
     VALIDATOR_EMAIL,
     VALIDATOR_MINLENGTH
 } from '../../shared/util/Validator';
-import Card from '../../shared/components/UIElement/Card';
 import { useForm } from '../../shared/hooks/form-hook';
-
 import '../../scss/Authenticate.scss';
 
+const Auth = () => {
+    const [formState, inputHandler] = useForm(
+        {
+            email: {
+                value: '',
+                isValid: false
+            },
+            password: {
+                value: '',
+                isValid: false
+            }
+        },
+        false
+    );
 
-const Authenticate = () => {
+    const authSubmitHandler = event => {
+        event.preventDefault();
+        console.log(formState.inputs);
+    };
+
     return (
         <Card className="authentication">
             <h2>Login Required</h2>
             <hr />
-            <form>
+            <form onSubmit={authSubmitHandler}>
                 <Input
-                    id="email"
                     element="input"
+                    id="email"
                     type="email"
-                    label="E-mail"
-                    validator={[VALIDATOR_EMAIL(), VALIDATOR_REQUIRE()]}
-                    errorText="Please enter a valid email address"
+                    label="E-Mail"
+                    validators={[VALIDATOR_EMAIL()]}
+                    errorText="Please enter a valid email address."
+                    onInput={inputHandler}
                 />
+                <Input
+                    element="input"
+                    id="password"
+                    type="password"
+                    label="Password"
+                    validators={[VALIDATOR_MINLENGTH(5)]}
+                    errorText="Please enter a valid password, at least 5 characters."
+                    onInput={inputHandler}
+                />
+                <Button type="submit" disabled={!formState.isValid}>
+                    LOGIN
+                </Button>
             </form>
         </Card>
-    )
-}
+    );
+};
 
-export default Authenticate;
+export default Auth;
